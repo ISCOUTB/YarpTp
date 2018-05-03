3cedd825a5e98b8bf5b5f9cdf417db20fa1144a6
kpedrozag <32544147+kpedrozag@users.noreply.github.com>
2018-05-03 11:39:36 -0500

    Agregado token y respuestas json

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-"""
-It's an API
-"""
-#app.config_frompyfile('config.py')
-from flask import Flask, session, redirect, url_for, escape, request
+from flask import Flask, session, redirect, url_for, escape, request, jsonify
+import hashlib
+from random import randrange
+
 app = Flask(__name__)
-# Set the secret key to some random bytes. Keep this really secret!
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 app.one_request = True
 
 @app.before_request
 def before_request():
     if 'token' in session and request.endpoint in ['login']:
-        return "Ya estas logueado"
-
+        return jsonify(message='Ya estas logueado')
 
 @app.route('/')
 def index():
@@ -20,21 +17,15 @@ def index():
 
 @app.route('/login')
 def login():
-
-    if 'token' in session:
-        return
-
     if app.one_request:
         # Primer usuario
-        session['token'] = "sadasda"
+        session['token'] = hashlib.md5(str(randrange(2**15)).encode()).hexdigest()
         app.one_request = False
     if not app.one_request:
         try:
-            text = 'Logged in - token = {}'.format(session['token'])
-            return text
+            return jsonify(token = session['token'])
         except KeyError:
-            return 'Ya existe un usuario loggeado'
-
+            return jsonify(message='Ya existe un usuario loggeado')
 
 @app.route('/logout')
 def logout():
@@ -42,10 +33,9 @@ def logout():
         # remove the username from the session if it's there
         app.one_request = True
         session.pop('token', None)
-        return "Ha cerrado sesion"
+        return jsonify(message='Ha cerrado sesion')
     else:
-        return "No existe un usuario logueado"
-
+        return jsonify(message='No existe un usuario logueado')
 
 if __name__ == '__main__':
     app.run()
