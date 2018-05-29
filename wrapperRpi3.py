e5bbb790f607a1a52eb19553157b1709862549dc
kpdrozag <kevin.andres01@hotmail.com>
2018-05-29 14:43:23 -0500

    Actualizado el wrapper

--- wrapperRpi3.py
+++ wrapperRpi3.py
@@ -1,6 +1,3 @@
-"""
-aaaa
-"""
 import RPi.GPIO as GPIO
 from time import sleep
 
@@ -40,70 +37,152 @@ class Prototype:
         else:
             self.pwm_b.ChangeDutyCycle(sp)
 
-    def ForwardMotorA(self):
+    def ForwardMotorA(self, tm=0):
         # self.signals((self.input1,self.input2), (GPIO.LOW, GPIO.HIGH), True, 50)
-        GPIO.output(self.input1, GPIO.LOW)
-        GPIO.output(self.input2, GPIO.HIGH)
-        self.pwm_a.ChangeDutyCycle(50)
+        if tm == 0:
+            GPIO.output(self.input1, GPIO.LOW)
+            GPIO.output(self.input2, GPIO.HIGH)
+            self.pwm_a.ChangeDutyCycle(50)
+        else:
+            GPIO.output(self.input1, GPIO.LOW)
+            GPIO.output(self.input2, GPIO.HIGH)
+            self.pwm_a.ChangeDutyCycle(50)
+            sleep(tm)
+            GPIO.cleanup((self.input1, self.input2))
 
-    def ForwardMotorB(self):
+
+    def ForwardMotorB(self, tm=0):
         # self.signals((self.input3,self.input4), (GPIO.HIGH, GPIO.LOW), False, 50)
-        GPIO.output(self.input3, GPIO.HIGH)
-        GPIO.output(self.input4, GPIO.LOW)
-        self.pwm_b.ChangeDutyCycle(50)
+        if tm == 0:
+            GPIO.output(self.input3, GPIO.HIGH)
+            GPIO.output(self.input4, GPIO.LOW)
+            self.pwm_b.ChangeDutyCycle(50)
+        else:
+            GPIO.output(self.input3, GPIO.HIGH)
+            GPIO.output(self.input4, GPIO.LOW)
+            self.pwm_b.ChangeDutyCycle(50)
+            sleep(tm)
+            GPIO.cleanup((self.input3, self.input4))
 
-    def ReverseMotorA(self):
+    def ReverseMotorA(self, tm=0):
         # self.signals((self.input1,self.input2), (GPIO.HIGH, GPIO.LOW), True, 50)
-        GPIO.output(self.input1, GPIO.HIGH)
-        GPIO.output(self.input2, GPIO.LOW)
-        self.pwm_a.ChangeDutyCycle(50)
+        if tm == 0:
+            GPIO.output(self.input1, GPIO.HIGH)
+            GPIO.output(self.input2, GPIO.LOW)
+            self.pwm_a.ChangeDutyCycle(50)
+        else:
+            GPIO.output(self.input1, GPIO.HIGH)
+            GPIO.output(self.input2, GPIO.LOW)
+            self.pwm_a.ChangeDutyCycle(50)
+            sleep(tm)
+            GPIO.cleanup((self.input1, self.input2))
 
-    def ReverseMotorB(self):
+
+    def ReverseMotorB(self, tm=0):
         # self.signals((self.input3,self.input4), (GPIO.LOW, GPIO.HIGH), False, 50)
-        GPIO.output(self.input3, GPIO.LOW)
-        GPIO.output(self.input4, GPIO.HIGH)
-        self.pwm_b.ChangeDutyCycle(50)
+        if tm == 0:
+            GPIO.output(self.input3, GPIO.LOW)
+            GPIO.output(self.input4, GPIO.HIGH)
+            self.pwm_b.ChangeDutyCycle(50)
+        else:
+            GPIO.output(self.input3, GPIO.LOW)
+            GPIO.output(self.input4, GPIO.HIGH)
+            self.pwm_b.ChangeDutyCycle(50)
+            sleep(tm)
+            GPIO.cleanup((self.input3, self.input4))
 
-    def ForwardBoth(self):
-        self.ForwardMotorA()
-        self.ForwardMotorB()
+    def ForwardBoth(self, tm=0):
+        if tm == 0:
+            self.ForwardMotorA()
+            self.ForwardMotorB()
+        else:
+            self.ForwardMotorA()
+            self.ForwardMotorB()
+            sleep(tm)
+            GPIO.cleanup()
 
-    def ReverseBoth(self):
-        self.ReverseMotorA()
-        self.ReverseMotorB()
+    def ReverseBoth(self, tm=0):
+        if tm == 0:
+            self.ReverseMotorA()
+            self.ReverseMotorB()
+        else:
+            self.ReverseMotorA()
+            self.ReverseMotorB()
+            sleep(tm)
+            GPIO.cleanup()
 
     # Move motor A - Forward - with Speed = speed
-    def ForwardMotorAwSpeed(self, speed):
+    def ForwardMotorAwSpeed(self, speed, tm=0):
         # self.signals((self.input1,self.input2), (GPIO.LOW, GPIO.HIGH), True, speed)
-        GPIO.output(self.input1, GPIO.LOW)
-        GPIO.output(self.input2, GPIO.HIGH)
-        self.pwm_a.ChangeDutyCycle(speed)
+        if tm == 0:
+            GPIO.output(self.input1, GPIO.LOW)
+            GPIO.output(self.input2, GPIO.HIGH)
+            self.pwm_a.ChangeDutyCycle(speed)
+        else:
+            GPIO.output(self.input1, GPIO.LOW)
+            GPIO.output(self.input2, GPIO.HIGH)
+            self.pwm_a.ChangeDutyCycle(speed)
+            sleep(tm)
+            GPIO.cleanup((self.input1, self.input2))
 
-    def ForwardMotorBwSpeed(self, speed):
+    def ForwardMotorBwSpeed(self, speed, tm=0):
         # self.signals((self.input3,self.input4), (GPIO.HIGH, GPIO.LOW), False, speed)
-        GPIO.output(self.input3, GPIO.HIGH)
-        GPIO.output(self.input4, GPIO.LOW)
-        self.pwm_b.ChangeDutyCycle(speed)
+        if tm == 0:
+            GPIO.output(self.input3, GPIO.HIGH)
+            GPIO.output(self.input4, GPIO.LOW)
+            self.pwm_b.ChangeDutyCycle(speed)
+        else:
+            GPIO.output(self.input3, GPIO.HIGH)
+            GPIO.output(self.input4, GPIO.LOW)
+            self.pwm_b.ChangeDutyCycle(speed)
+            sleep(tm)
+            GPIO.cleanup((self.input3, self.input4))
 
-    def ReverseMotorAwSpeed(self, speed):
+    def ReverseMotorAwSpeed(self, speed, tm=0):
         # self.signals((self.input1,self.input2), (GPIO.HIGH, GPIO.LOW), True, speed)
-        GPIO.output(self.input1, GPIO.HIGH)
-        GPIO.output(self.input2, GPIO.LOW)
-        self.pwm_a.ChangeDutyCycle(speed)
+        if tm == 0:
+            GPIO.output(self.input1, GPIO.HIGH)
+            GPIO.output(self.input2, GPIO.LOW)
+            self.pwm_a.ChangeDutyCycle(speed)
+        else:
+            GPIO.output(self.input1, GPIO.HIGH)
+            GPIO.output(self.input2, GPIO.LOW)
+            self.pwm_a.ChangeDutyCycle(speed)
+            sleep(tm)
+            GPIO.cleanup((self.input1, self.input2))
 
-    def ReverseMotorBwSpeed(self, speed):
+    def ReverseMotorBwSpeed(self, speed, tm=0):
         # self.signals((self.input3,self.input4), (GPIO.LOW, GPIO.HIGH), False, speed)
-        GPIO.output(self.input3, GPIO.LOW)
-        GPIO.output(self.input4, GPIO.HIGH)
-        self.pwm_b.ChangeDutyCycle(speed)
+        if tm == 0:
+            GPIO.output(self.input3, GPIO.LOW)
+            GPIO.output(self.input4, GPIO.HIGH)
+            self.pwm_b.ChangeDutyCycle(speed)
+        else:
+            GPIO.output(self.input3, GPIO.LOW)
+            GPIO.output(self.input4, GPIO.HIGH)
+            self.pwm_b.ChangeDutyCycle(speed)
+            sleep(tm)
+            GPIO.cleanup((self.input3, self.input4))
 
-    def ForwardBothwSpeed(self, speed):
-        self.ForwardMotorAwSpeed(speed)
-        self.ForwardMotorBwSpeed(speed)
+    def ForwardBothwSpeed(self, speed, tm=0):
+        if tm == 0:
+            self.ForwardMotorAwSpeed(speed)
+            self.ForwardMotorBwSpeed(speed)
+        else:
+            self.ForwardMotorAwSpeed(speed)
+            self.ForwardMotorBwSpeed(speed)
+            sleep(tm)
+            GPIO.cleanup()
 
-    def ReverseBothwSpeed(self, speed):
-        self.ReverseMotorAwSpeed(speed)
-        self.ReverseMotorBwSpeed(speed)
+    def ReverseBothwSpeed(self, speed, tm=0):
+        if tm == 0:
+            self.ReverseMotorAwSpeed(speed)
+            self.ReverseMotorBwSpeed(speed)
+        else:
+            self.ReverseMotorAwSpeed(speed)
+            self.ReverseMotorBwSpeed(speed)
+            sleep(tm)
+            GPIO.cleanup()
 
     def StopAll(self):
         GPIO.cleanup()
